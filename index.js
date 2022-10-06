ae3411ab7bb1801d9a72af7535772f482ab7619f
javaharreddy <89154426+javaharreddy@users.noreply.github.com>
2022-10-06 16:01:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+for(var i=0;i<document.querySelectorAll('button').length;i++){
+    document.querySelectorAll('button')[i].addEventListener('click',gotclicked);
+};
+window.addEventListener('keydown',e => {
+    play(e.key);
+})
+function gotclicked(){
+    this.style.color = 'black';
+    var key = this.innerHTML;
+    play(key);
+}
+function play(key){
+    var audio = new Audio('sounds/crash.mp3');
+    if(key === 'w' ){
+    audio = new Audio('sounds/crash.mp3');
+    }
+    else if(key==='a'){
+        audio = new Audio('sounds/kick-bass.mp3');
+    }
+    else if(key==='s'){
+        audio = new Audio('sounds/snare.mp3');
+    }
+    else if(key==='d'){
+        audio = new Audio('sounds/tom-1.mp3');
+    }
+    else if(key==='j'){
+        audio = new Audio('sounds/tom-2.mp3');
+    }
+    else if(key==='k'){
+        audio = new Audio('sounds/tom-3.mp3');
+    }
+    else if(key==='l'){
+        audio = new Audio('sounds/tom-4.mp3');
+    }
+    audio.play();
+}
